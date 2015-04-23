a57c29864deb4bf2f3ab9444313e043116c5b815
Alexander Shushunov <AShushunov@mail.ru>
2015-04-23 11:14:46 +0300

    small refactoring

--- wise-men.js
+++ wise-men.js
@@ -1,6 +1,6 @@
 "use strict";
 (function () {
-	var allVariants = (function allVariants() {
+	var allCombination = (function allCombination() {
 		var numbers = [];
 		for (var first = 2; first < 100; first++) {
 			for (var second = 2; second < 100; second++) {
@@ -27,26 +27,26 @@
 		return ret;
 	})();
 
-	var oneNotPrimeVariants = (function oneNotPrime() {
-		return allVariants.filter(function (item) {
+	var oneNotPrimeCombination = (function oneNotPrimeCombination() {
+		return allCombination.filter(function (item) {
 			return !(isPrime(item.first) && isPrime(item.second));
 		});
 	})();
 
-	var sumDoesNotLookLikePrimesSum = (function sumDoesNotLookLikePrimesSum() {
-		return allVariants.filter(function (item) {
+	var sumDoesNotLookLikePrimesSumCombination = (function sumDoesNotLookLikePrimesSumCombination() {
+		return allCombination.filter(function (item) {
 			return allPrimesSum.indexOf(item.sum) == -1;
 		});
 	})();
 
 	var valiVariants = (function valiVariants() {
-		return _.uniq(sumDoesNotLookLikePrimesSum.map(function (item) {
+		return _.uniq(sumDoesNotLookLikePrimesSumCombination.map(function (item) {
 			return item.sum;
 		}));
 	})();
 
 	var aliVariants = (function aliVariants() {
-		return _.uniq(oneNotPrimeVariants.map(function (item) {
+		return _.uniq(oneNotPrimeCombination.map(function (item) {
 			return item.mult;
 		}));
 	})();
@@ -77,7 +77,7 @@
 	var answer = (function answer() {
 		var sum = valiVariantsAfterSecondAliAnswer[0];
 		var mult = _.intersection(aliVariantsAfterValiAnswer, getPartsMult(sum))[0];
-		return oneNotPrimeVariants.filter(function (item) {
+		return oneNotPrimeCombination.filter(function (item) {
 			return item.sum == sum && item.mult == mult;
 		})[0];
 	})();
